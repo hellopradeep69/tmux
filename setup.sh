918f1d650e25fcd6544e832d7a9b294543911143
k.pradeep <hellopradeep69@gmail.com>
2025-10-28 00:24:31 +0530

    made script work:test

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-Install_tmux
+Install_tmux() {
+    touch ~/.tmux.conf
+    mkdir ~/tmux-temp
+    cp ~/tmux-temp/.tmux.conf ~/.tmux.conf
+    rm -rf ~/tmux-temp
+}
 
-mkdir ~/tmux-temp
-cp ~/tmux-temp/.tmux.conf ~/.tmux.conf
-rm -rf ~/tmux-temp
+Install_tmux

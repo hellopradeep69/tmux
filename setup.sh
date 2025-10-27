8bdeab1daac3816f1139b9ec19b1fac7d5747603
k.pradeep <hellopradeep69@gmail.com>
2025-10-28 00:28:26 +0530

    made script work:test

--- setup.sh
+++ setup.sh
@@ -3,8 +3,10 @@
 Install_tmux() {
     touch ~/.tmux.conf
     mkdir ~/tmux-temp
+    git clone https://github.com/hellopradeep69/tmux.git ~/tmux-temp
     cp ~/tmux-temp/.tmux.conf ~/.tmux.conf
     rm -rf ~/tmux-temp
+    rm -rf ~/tmux/
 }
 
 Install_tmux
